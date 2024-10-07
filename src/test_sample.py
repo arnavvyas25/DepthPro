5c079a47a39d21b5c47e51d0cf0fc2bd4bd38e84
Arnav Vyas <arnavvyas25@gmail.com>
2024-10-07 02:42:01 -0400

    Added test_sample.ipynb

--- src/test_sample.py
+++ src/test_sample.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import depth_pro
+import matplotlib.pyplot as plt
 
-image_path = "path/to/your/image.jpg"
+image_path = "src/input_images/IMG_4272.jpg"
 
 # Load model and preprocessing transform
 model, transform = depth_pro.create_model_and_transforms()
@@ -16,9 +17,16 @@ prediction = model.infer(image, f_px=f_px)
 depth = prediction["depth"]  # Depth in [m].
 focallength_px = prediction["focallength_px"]  # Focal length in pixels.
 
-# for a depth-based dataset
-boundary_f1 = SI_boundary_F1(predicted_depth, target_depth)
+# plot depth map and image
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(Image.open(image_path))
+plt.title("Image")
+plt.axis("off")
 
-# output depth as image
-depth_image = depth_pro.depth_to_image(depth)
-depth_image.save("path/to/your/output/depth_image.jpg")
+plt.subplot(1, 2, 2)
+plt.imshow(depth, cmap="inferno")
+plt.title("Depth map")
+plt.axis("off")
+
+plt.show()
